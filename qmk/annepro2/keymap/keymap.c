c8d6521e6e7da2771ca3865e40aaf6f882b1eb74
David Elentok <3david@gmail.com>
2023-09-07 18:43:18 +0300

    AnnePro2: add flash script + home row mods

--- qmk/annepro2/keymap/keymap.c
+++ qmk/annepro2/keymap/keymap.c
@@ -54,9 +54,9 @@ enum anne_pro_layers {
  [BASE] = LAYOUT_60_ansi( /* Base */
     QK_GESC,          KC_1,         KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
     KC_TAB,           KC_Q,         KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    KC_LCTL,          LCTL_T(KC_A), KC_S,    KC_D, LT(FN1, KC_F), KC_G, KC_H, LT(FN1, KC_J),  KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
+    KC_LCTL,          LCTL_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LT(FN1, KC_F), KC_G, KC_H, LT(FN1, KC_J),  RSFT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN),          KC_QUOT,          KC_ENT,
     KC_LSFT,          KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL,          KC_LALT, KC_LGUI,                    LGUI_T(KC_SPC),             KC_RALT, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+    KC_LCTL,          KC_LALT, KC_LGUI,                    KC_SPC,             KC_RALT, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
 ),
   /*
   * Layer FN1
@@ -75,9 +75,9 @@ enum anne_pro_layers {
   */
  [FN1] = LAYOUT_60_ansi( /* FN1 */
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_PGUP, KC_VOLU, KC_HOME, _______, KC_PSCR, KC_HOME, KC_END,  _______,
-    _______, _______, _______, KC_LPRN, KC_RPRN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
-    _______,          _______, KC_LCBR, KC_RCBR, _______, _______, KC_PGDN, KC_VOLD, _______, KC_INS,  KC_DEL,  _______,
+    _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_PGUP, KC_VOLU, KC_HOME, _______, KC_PSCR, KC_HOME, KC_END,  _______,
+    _______, _______, KC_LPRN, KC_RPRN, _______, KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
+    _______,          KC_LCBR, KC_RCBR, _______, _______, KC_PGDN, KC_VOLD, KC_END,  _______, KC_INS,  KC_DEL,  _______,
     _______, _______, _______,                            _______,                   _______, _______, MO(FN2), _______
 ),
   /*
