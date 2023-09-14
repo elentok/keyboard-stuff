feaf9463d5196e9e42197e2d6a15a825e88ac439
David Elentok <3david@gmail.com>
2023-09-14 21:56:56 +0300

    Sinc: add layer enum

--- qmk/sinc-rev3/keymap.c
+++ qmk/sinc-rev3/keymap.c
@@ -17,7 +17,10 @@
 #define RGBLIGHT_LIMIT_VAL 100
 #define RGBLIGHT_SPLIT
 
-#define L1 1
+enum sinc_layers {
+  BASE,
+  L1,
+};
 
 // Left home row mods
 #define LHRM1 MOD_LCTL
@@ -37,16 +40,16 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_80_with_macro(
+  [BASE] = LAYOUT_80_with_macro(
     KC_MUTE,             /*HOLE*/             KC_ESC,  KC_F1,           KC_F2,           KC_F3,           KC_F4,          KC_F5,          KC_F6,  KC_F7,           KC_F8,           KC_F9,           KC_F10,             KC_F11,  KC_F12,           KC_DEL,  KC_INS,
     MY_KC_LOCK,          KC_MEDIA_PLAY_PAUSE, KC_GRV,  KC_1,            KC_2,            KC_3,            KC_4,           KC_5,           KC_6,   KC_7,            KC_8,            KC_9,            KC_0,               KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
     KC_MEDIA_PLAY_PAUSE, KC_VOLU,             KC_TAB,  KC_Q,            KC_W,            KC_E,            KC_R,           KC_T,           KC_Y,   KC_U,            KC_I,            KC_O,            KC_P,               KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
-    KC_F5,               KC_VOLD,             KC_LCTL, MT(LHRM1, KC_A), MT(LHRM2, KC_S), MT(LHRM3, KC_D), LT(1, KC_F),    KC_G,           KC_H,   LT(1, KC_J), MT(RHRM3, KC_K), MT(RHRM2, KC_L), MT(RHRM1, KC_SCLN), KC_QUOT, KC_ENT,  KC_PGUP,
+    KC_F5,               KC_VOLD,             KC_LCTL, MT(LHRM1, KC_A), MT(LHRM2, KC_S), MT(LHRM3, KC_D), LT(L1, KC_F),  KC_G,           KC_H,   LT(L1, KC_J), MT(RHRM3, KC_K), MT(RHRM2, KC_L), MT(RHRM1, KC_SCLN), KC_QUOT, KC_ENT,  KC_PGUP,
     KC_F7,               KC_F8,               KC_LSFT, KC_Z,            KC_X,            KC_C,            KC_V,           KC_B,           KC_N,   KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,            KC_RSFT, KC_UP,   KC_PGDN,
-    KC_F9,               KC_F10,              KC_LCTL, MO(1),           KC_LALT,         KC_LGUI,         LSFT_T(KC_SPC), RSFT_T(KC_SPC), MO(1),  RSFT_T(KC_SPC),  KC_RGUI,         MO(1),           KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F9,               KC_F10,              KC_LCTL, MO(L1),         KC_LALT,         KC_LGUI,         LSFT_T(KC_SPC), RSFT_T(KC_SPC), MO(L1),  RSFT_T(KC_SPC),  KC_RGUI,         MO(L1),           KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [1] = LAYOUT_80_with_macro(
+  [L1] = LAYOUT_80_with_macro(
     QK_CLEAR_EEPROM,     _______, _______,      _______,      _______,      _______,     _______,    _______, _______,     _______,      _______,      _______,  _______, _______, _______, _______,
     RGB_HUI,             RGB_HUD,             QK_GESC, KC_F1,        KC_F2,        KC_F3,        KC_F4,       KC_F5,      KC_F6,   KC_F7,       KC_F8,        KC_F9,        KC_F10,   KC_F11,  KC_F12,  _______, _______, _______,
     RGB_SAI,             RGB_SAD,             RGB_TOG, RGB_MOD,      KC_LBRC,      KC_RBRC,      KC_TAB,      _______,    _______, _______,     _______,      _______,      _______,  _______, _______, _______, _______,
