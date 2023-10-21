e127030137f7e7dfa98a4b7905b477a14545efe0
David Elentok <3david@gmail.com>
2023-10-21 23:18:33 +0300

    Hillside: it's working!

--- qmk/hillside46/config.h
+++ qmk/hillside46/config.h
@@ -7,3 +7,12 @@
 // Such as Elite-C v3, but not v3.1 on. Also apparently some ProMicro boards.
 // For those boands, use usb detection instead.
 // #define SPLIT_USB_DETECT
+
+// Identify Left/Right properly
+#undef SPLIT_HAND_MATRIX_GRID
+#define EE_HANDS
+
+// Default LED colors
+#define RGBLIGHT_DEFAULT_HUE 128
+#define RGBLIGHT_DEFAULT_SAT 255
+#define RGBLIGHT_DEFAULT_VAL 80
