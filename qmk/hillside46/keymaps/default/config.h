7b4b49f5adec27848bb69e28d3e89be2f158dbc8
David Elentok <3david@gmail.com>
2023-10-27 14:43:30 +0300

    Add all of the hillside46 code in here

---
+++ qmk/hillside46/keymaps/default/config.h
@@ -0,0 +1,12 @@
+// Copyright 2022 Michael McCoyd (@mmccoyd)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// #undef SPLIT_HAND_MATRIX_GRID
+// #define EE_HANDS
+
+// Some boards have issues detecting handedness using the vbus voltage.
+// Such as Elite-C v3, but not v3.1 on. Also apparently some ProMicro boards.
+// For those boands, use usb detection instead.
+// #define SPLIT_USB_DETECT
