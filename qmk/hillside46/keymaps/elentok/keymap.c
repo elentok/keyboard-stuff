6703f162dcff6df14771d47ceae63607df16b6b3
David Elentok <3david@gmail.com>
2023-10-29 22:08:44 +0200

    Hillside: set nav layer hjkl as arrows

--- qmk/hillside46/keymaps/elentok/keymap.c
+++ qmk/hillside46/keymaps/elentok/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+// #include "elentok.h"
 
 enum layers {
     _QWERTY = 0,
@@ -37,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NAV] = LAYOUT(
      COPY   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-     CUT    , KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, PASTE  ,                                        KC_PGUP, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_PGDN,
+     CUT    , KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, PASTE  ,                                        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PGDN, KC_PGDN,
      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , UNDO   ,                      REDO   , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_RCTL,
                                          _______, _______, _______, _______,    LY_ADJ , _______, _______, _______
     ),
