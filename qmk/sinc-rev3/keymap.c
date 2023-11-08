bf48a9bd599c79459b2dbefc2f0f28a3faa2d21b
David Elentok <3david@gmail.com>
2023-11-08 21:43:46 +0200

    Sinc: change color by layer type

--- qmk/sinc-rev3/keymap.c
+++ qmk/sinc-rev3/keymap.c
@@ -3,29 +3,21 @@
 
 #include QMK_KEYBOARD_H
 #include "elentok.h"
-// #include "quantum/rgblight/rgblight.h"
-// #include "quantum.h"
-// #include "split_util.h"
-// #include "rgblight.h"
-
-// #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
-// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
-// #define RGBLIGHT_DEFAULT_HUE 128
-// #define RGBLIGHT_DEFAULT_SAT 255
-// #define RGBLIGHT_DEFAULT_VAL 100
-// #define RGBLIGHT_LIMIT_VAL 100
-// #define RGBLIGHT_SPLIT
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   ELC_LOCK,
+  ELC_RED,
+  ELC_NO_RED,
 };
 
+// less bright version
+#define HSV_CYAN1       128, 255, 100
+
 void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
-  rgb_matrix_set_color_all(0, 255, 203);
+  rgb_matrix_enable_noeeprom();
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+  rgb_matrix_sethsv_noeeprom(HSV_CYAN1);
 }
 
 // KC_MPLY = KC_MEDIA_PLAY_PAUSE
@@ -57,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_HUI,   RGB_HUD, /*|*/ QK_GESC, KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,   KC_F6,   /*|*/ KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  _______, _______, QK_BOOT,
     RGB_SAI,   RGB_SAD, /*|*/ KC_ESC,  EL_LJ_Q, KC_COLON, KC_LPRN, KC_RPRN,  EL_LJ_T,          /*|*/ _______, _______, _______,  _______,  _______, _______, _______, _______, QK_REBOOT,
     RGB_VAI,   RGB_VAD, /*|*/ _______, _______, _______,  KC_LBRC, KC_RBRC,  EL_LJ_G,          /*|*/ _______, _______, _______,  _______, _______, EL_LJ_QUOT,  _______,      _______,
+    RGB_TOG,   RGB_MOD, /*|*/ ELC_RED, ELC_NO_RED, _______,  KC_LCBR, KC_RCBR,  _______,          /*|*/ EL_LJ_N, _______, TO_NORM,  _______,  _______,     _______,      _______, _______,
     QK_REBOOT, _______, /*|*/ _______, _______, _______,  _______,           ELC_LOCK,         /*|*/ _______, _______, _______,  _______,  _______, _______, _______, _______, _______
-    RGB_TOG,   RGB_MOD, /*|*/ _______, _______, _______,  KC_LCBR, KC_RCBR,  _______,          /*|*/ EL_LJ_N, _______, TO_NORM,  _______,  _______,     _______,      _______, _______,
   ),
 
   // Gaming layer (no dual-mode keys)
@@ -102,68 +94,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //),
 };
 
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//   for (uint8_t i = led_min; i < led_max; i++) {
-//     HSV hsv = {0, 0, 100}; // white
-//
-//     switch(get_highest_layer(layer_state|default_layer_state)) {
-//       case 2:
-//         // green
-//         hsv.h = 85;
-//         hsv.s = 255;
-//         break;
-//       case 1:
-//         // cyan
-//         hsv.h = 128;
-//         hsv.s = 255;
-//         break;
-//       default:
-//         break;
-//     }
-//
-//     RGB rgb = hsv_to_rgb(hsv);
-//     rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-//   }
-//   // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
-//   return false;
-// }
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch(get_highest_layer(state)) {
+  case VI_NORM:
+    rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+    break;
+  case VI_VIS:
+    rgb_matrix_sethsv_noeeprom(HSV_BLUE);
+    break;
+  default:
+    rgb_matrix_sethsv_noeeprom(HSV_CYAN1);
+    break;
+  }
 
-// void eeconfig_init_kb(void) {
-//   rgblight_enable();
-//   rgblight_sethsv(HSV_BLUE);
-//   rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
-// }
+  return state;
+}
 
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//   switch(biton32(state)) {
-//   case 0:
-//     // Green
-//     rgblight_enable();
-//     // rgblight_sethsv(HSV_GREEN);
-//     rgblight_sethsv(HSV_GREEN);
-//     break;
-//   case 1:
-//     // Red
-//     rgblight_enable();
-//     // rgblight_sethsv(HSV_RED);
-//     rgblight_sethsv(HSV_RED);
-//     break;
-//   default:
-//     // Yellow
-//     rgblight_enable();
-//     rgblight_sethsv(HSV_YELLOW);
-//     // rgblight_sethsv(HSV_YELLOW);
-//     break;
-//   }
-//   return state;
-// }
-//
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ELC_LOCK:
       if (!record->event.pressed) {
         // SEND_STRING(SS_DOWN(LGUI) SS_LCTL("q") SS_UP(LGUI));
         SEND_STRING(SS_LCTL(SS_LGUI("q")));
+      }
+      break;
+    case ELC_RED:
+      if (record->event.pressed) {
+        rgb_matrix_sethsv_noeeprom(HSV_RED);
+      }
+      break;
+    case ELC_NO_RED:
+      if (record->event.pressed) {
+        rgb_matrix_sethsv_noeeprom(HSV_CYAN);
       }
       break;
   }
