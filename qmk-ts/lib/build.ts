84935d50cfef2a60db65c11e446f036f75649b9d
David Elentok <3david@gmail.com>
2023-12-13 11:02:43 +0200

    Cleanup

--- qmk-ts/lib/build.ts
+++ qmk-ts/lib/build.ts
@@ -35,7 +35,8 @@ export function build<T extends string>(keyboard: Keyboard<T>, layout: Layout<T>
   for (const layer of builtLayers) {
     lines.push(`[${layer.name}] = LAYOUT(`)
     for (const row of layer.mappings) {
-      lines.push(row.map((mapping) => mapping.padEnd(maxCellWidth)).join(", "))
+      const line = row.map((mapping) => `${mapping}, `.padEnd(maxCellWidth + 2)).join("").replace(/^, /, "")
+      lines.push(`  ${line}`)
     }
   }
 
@@ -43,27 +44,14 @@ export function build<T extends string>(keyboard: Keyboard<T>, layout: Layout<T>
 }
 
 function buildLayer<T extends string>(keyboard: Keyboard<T>, name: LayerName, layer: Layer<T>): BuiltLayer {
-  // const lines = []
   const keyToMapping = { ...layer.row1, ...layer.row2, ...layer.row3, ...layer.thumb, ...layer.extra }
   const mappings = keyboard.rows.map((row) => {
     return row.map((key) => {
       const mapping = keyToMapping[key] ?? "_______"
       const aliasedMapping = expandAlias(mapping)
       return aliasedMapping ?? mapping
-
-      // console.log(mapping)
-      // Deno.writeSync(Deno.stdout.rid, newmapping)
-      // Deno.stdout.writeSync(textEncoder.encode(`${mapping}, `))
     })
   })
-
-  // for (const row of mappedLayer) {
-  //   lines.push(row.join(", "))
-  //   // for (const mapping of row) {
-  //   //
-  //   //   Deno.stdout.writeSync(textEncoder.encode(`${mapping}, `))
-  //   // }
-  // }
 
   return { name, mappings }
 }
