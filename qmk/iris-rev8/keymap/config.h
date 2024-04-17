f8a3acafa678f9c7f0e264937fa6dff2e574a98c
David Elentok <3david@gmail.com>
2024-04-17 09:55:46 +0300

    Iris: combo changes

--- qmk/iris-rev8/keymap/config.h
+++ qmk/iris-rev8/keymap/config.h
@@ -1,9 +1,9 @@
 #define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
 // #define TAPPING_TERM 200
-
+// HOLD_ON_OTHER_KEY_PRESS
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 // #define QUICK_TAP_TERM 0
 
 #undef COMBO_COUNT
-#define COMBO_COUNT 15
+#define COMBO_COUNT 13
--- qmk/iris-rev8/keymap/keymap.c
+++ qmk/iris-rev8/keymap/keymap.c
@@ -145,9 +145,9 @@ const uint16_t PROGMEM df_combo[] = {LGUI_T(KC_D), LT(LF, KC_F), COMBO_END};
 // const uint16_t PROGMEM fg_combo[] = {LT(LF, KC_F), KC_G, COMBO_END};
 const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM ef_combo[] = {KC_E, LT(LF, KC_F), COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {LSFT_T(KC_S), LGUI_T(KC_D), COMBO_END};
+// const uint16_t PROGMEM sd_combo[] = {LSFT_T(KC_S), LGUI_T(KC_D), COMBO_END};
 // const uint16_t PROGMEM sdf_combo[] = {LSFT_T(KC_S), LGUI_T(KC_D), LT(LF, KC_F), COMBO_END};
-const uint16_t PROGMEM sf_combo[] = {LSFT_T(KC_S), LT(LF, KC_F), COMBO_END};
+// const uint16_t PROGMEM sf_combo[] = {LSFT_T(KC_S), LT(LF, KC_F), COMBO_END};
 const uint16_t PROGMEM jk_combo[] = {LT(LJ, KC_J), RGUI_T(KC_K), COMBO_END};
 const uint16_t PROGMEM jl_combo[] = {LT(LJ, KC_J), RSFT_T(KC_L), COMBO_END};
 const uint16_t PROGMEM uio_combo[] = {KC_U, KC_I, KC_O, COMBO_END};
@@ -157,7 +157,7 @@ const uint16_t PROGMEM wf_combo[] = {KC_W, LT(LF, KC_F), COMBO_END};
 const uint16_t PROGMEM ji_combo[] = {LT(LJ, KC_J), KC_I, COMBO_END};
 const uint16_t PROGMEM jo_combo[] = {LT(LJ, KC_J), KC_O, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM mcomma_combo[] = {LT(LM, KC_M), LALT_T(KC_COMM), COMBO_END};
+const uint16_t PROGMEM mcommadot_combo[] = {LT(LM, KC_M), LALT_T(KC_COMM), KC_DOT, COMBO_END};
 // const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 // const uint16_t PROGMEM comma_dot_io_combo[] = {LALT_T(KC_COMM), KC_DOT, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
@@ -166,9 +166,9 @@ combo_t key_combos[COMBO_COUNT] = {
     // COMBO(fg_combo, KC_DLR),
     COMBO(er_combo, KC_LPRN),
     COMBO(ef_combo, KC_LBRC),
-    COMBO(sd_combo, LCTL(KC_A)), // keycodes with modifiers are possible too!
+    // COMBO(sd_combo, LCTL(KC_A)), // keycodes with modifiers are possible too!
     // COMBO(sdf_combo, LGUI(KC_SPC)),
-    COMBO(sf_combo, LGUI(KC_SPC)),
+    // COMBO(sf_combo, LGUI(KC_SPC)),
     COMBO(jk_combo, KC_ESC),
     COMBO(jl_combo, KC_COLON),
     COMBO(uio_combo, LCTL(LSFT(LGUI(KC_I)))),
@@ -178,7 +178,7 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(ji_combo, KC_MINS),
     COMBO(jo_combo, KC_EQL),
     COMBO(io_combo, KC_UNDS),
-    COMBO(mcomma_combo, LGUI(KC_1)),
+    COMBO(mcommadot_combo, LGUI(KC_1)),
     // COMBO(ui_combo, LGUI(KC_1)),
     // COMBO(comma_dot_io_combo, LGUI(KC_1)),
 };
