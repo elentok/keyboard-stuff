c4e6f6a439f47f42587c7fb7afd35f64baa7e0a4
David Elentok <3david@gmail.com>
2024-05-30 23:48:30 +0300

    quefrency: get it to build

--- qmk/quefrency-rev4/keymap.c
+++ qmk/quefrency-rev4/keymap.c
@@ -13,43 +13,43 @@
 //     _FN1,
 // };
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  ELC_LOCK,
-};
+// enum custom_keycodes {
+//   QWERTY = SAFE_RANGE,
+//   ELC_LOCK,
+// };
 
 // KC_MPLY = KC_MEDIA_PLAY_PAUSE
 // EE_CLR = QK_CLEAR_EEPROM - Reinitializes the keyboard's EEPROM (persistent memory)                                                                                          |
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_65_with_macro(
+  [L1] = LAYOUT_65_with_macro(
     KC_F1,                 KC_ESC,   /*|*/ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    /*|*/ KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, KC_EQL,     KC_DEL,  KC_BSPC, KC_HOME,
     KC_MEDIA_PLAY_PAUSE,   KC_VOLU,  /*|*/ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             /*|*/ KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS, KC_END,
-    KC_F5,                 KC_VOLD,  /*|*/ KC_LGUI, EL_A,    EL_S,    EL_D,    EL_F,    KC_G,             /*|*/ KC_H,    EL_J,    EL_K,     EL_L,     EL_SCLN, KC_QUOT,             KC_ENT,  KC_PGUP,
+    KC_F5,                 KC_VOLD,  /*|*/ KC_LGUI, E_A,    E_S,    E_D,    E_F,    KC_G,             /*|*/ KC_H,    E_J,    E_K,     E_L,     E_SCLN, KC_QUOT,             KC_ENT,  KC_PGUP,
     KC_F7,                 KC_F8,    /*|*/ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             /*|*/ KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,             KC_UP,   KC_PGDN,
-    KC_F9,                 ELC_LOCK, /*|*/ KC_LCTL, MO(LF),  KC_LALT, KC_LGUI, EL_LSPC, _______,          /*|*/ EL_RSPC, MO(LJ),  KC_RCTL,  _______,                       KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F9,                 EC_LOCK, /*|*/ KC_LCTL, MO(LF),  KC_LALT, KC_LGUI, KC_SPC, _______,          /*|*/ KC_ENT, MO(LJ),  KC_RCTL,  _______,                       KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [LF] = LAYOUT_65_with_macro(
     _______,               _______,  /*|*/ KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   /*|*/ KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,     KC_DEL,  KC_BSPC, QK_BOOT,
-    RGB_TOG,               _______,  /*|*/ _______, EL_LF_Q, KC_COLN, KC_RBRC, _______, _______,          /*|*/ _______, KC_PGUP, _______,  _______,  _______, _______,    _______, _______, QK_REBOOT,
-    RGB_MOD,               _______,  /*|*/ _______, _______, _______, _______, _______, EL_LF_G,          /*|*/ KC_LEFT, KC_DOWN, KC_UP  ,  KC_RIGHT, _______, EL_LF_QUOT,          _______, _______,
-    _______,               _______,  /*|*/ _______, _______, _______, _______, _______, _______,          /*|*/ EL_LF_N, EL_LF_M,    _______,  _______,  _______, _______,             DT_UP  , _______,
-    _______,               _______,  /*|*/ _______, _______, _______, _______, EL_LF_LSPC, _______,       /*|*/ EL_LF_RSPC, _______, _______,  _______,                       _______, DT_DOWN, DT_PRNT
+    RGB_TOG,               _______,  /*|*/ _______, EF_Q, KC_COLN, KC_RBRC, _______, _______,          /*|*/ _______, KC_PGUP, _______,  _______,  _______, _______,    _______, _______, QK_REBOOT,
+    RGB_MOD,               _______,  /*|*/ _______, _______, _______, _______, _______, EF_G,          /*|*/ KC_LEFT, KC_DOWN, KC_UP  ,  KC_RIGHT, _______, EF_QUOT,          _______, _______,
+    _______,               _______,  /*|*/ _______, _______, _______, _______, _______, _______,          /*|*/ EF_N, EF_M,    _______,  _______,  _______, _______,             DT_UP  , _______,
+    _______,               _______,  /*|*/ _______, _______, _______, _______, EF_LSPC, _______,       /*|*/ EF_RSPC, _______, _______,  _______,                       _______, DT_DOWN, DT_PRNT
   ),
 
   [LJ] = LAYOUT_65_with_macro(
     _______,               _______,  /*|*/ KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   /*|*/ KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,     KC_DEL,  KC_BSPC, QK_BOOT,
-    RGB_TOG,               _______,  /*|*/ _______, EL_LJ_Q, KC_LBRC, KC_RBRC, _______, _______,          /*|*/ _______, _______, ELC_LOCK, _______,  _______, _______,    _______, _______, QK_REBOOT,
-    RGB_MOD,               _______,  /*|*/ _______, _______, KC_LPRN, KC_RPRN, _______, EL_LJ_G,          /*|*/ _______, _______, _______,  _______, _______, EL_LF_QUOT,          _______, _______,
-    _______,               _______,  /*|*/ _______, _______, KC_LCBR, KC_RCBR, _______, _______,          /*|*/ EL_LJ_N, EL_LJ_M, _______,  _______,  _______, _______,             _______, _______,
+    RGB_TOG,               _______,  /*|*/ _______, EJ_Q, KC_LBRC, KC_RBRC, _______, _______,          /*|*/ _______, _______, EC_LOCK, _______,  _______, _______,    _______, _______, QK_REBOOT,
+    RGB_MOD,               _______,  /*|*/ _______, _______, KC_LPRN, KC_RPRN, _______, EJ_G,          /*|*/ _______, _______, _______,  _______, _______, EF_QUOT,          _______, _______,
+    _______,               _______,  /*|*/ _______, _______, KC_LCBR, KC_RCBR, _______, _______,          /*|*/ EJ_N, EJ_M, _______,  _______,  _______, _______,             _______, _______,
     _______,               _______,  /*|*/ _______, _______, _______, _______, _______, _______,          /*|*/ _______, _______, _______,  _______,                       _______, _______, _______
   )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ELC_LOCK:
+    case EC_LOCK:
       if (!record->event.pressed) {
         // SEND_STRING(SS_DOWN(LGUI) SS_LCTL("q") SS_UP(LGUI));
         SEND_STRING(SS_LCTL(SS_LGUI("q")));
